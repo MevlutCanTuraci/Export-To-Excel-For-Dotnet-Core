499895a374427257cf0bfcfdc26d38821269dc00
User <User@DESKTOP-IKONQE5>
2024-05-29 14:37:30 +0300

    Added generic function

--- App.ExcelExport/ExcelHelper.cs
+++ App.ExcelExport/ExcelHelper.cs
@@ -1,6 +1,8 @@
 ﻿#region Imports
 using ClosedXML.Excel;
+using DocumentFormat.OpenXml.Spreadsheet;
 using GenFu;
+using System;
 #endregion
 
 
@@ -8,6 +10,16 @@ namespace App.ExcelExport
 {
     public class ExcelHelper
     {
+        private Dictionary<string, int>? _headerMapping;
+
+        //Only for generic funcs
+        public void ConfigureHeaders(Dictionary<string, int> headerMapping)
+        {
+            //_headerMapping = new Dictionary<string, int>();
+            _headerMapping = headerMapping;
+        }
+
+
         private static IEnumerable<EmployeeModel> GenerateData()
         {
             GenFu.GenFu.Configure<EmployeeModel>()
@@ -20,7 +32,6 @@ namespace App.ExcelExport
 
             return A.ListOf<EmployeeModel>();
         }
-
 
         public static void ExportToTable(string excelOutPath)
         {
@@ -84,7 +95,6 @@ namespace App.ExcelExport
             }
         }
 
-
         public static void ExportToDefalt(string excelOutPath)
         {
             var users = GenerateData();
@@ -137,6 +147,88 @@ namespace App.ExcelExport
                 }
             }
         }
+
+        public static void ExportWithUseGeneric(string excelOutPath)
+        {
+            var users = GenerateData();
+
+            ExcelHelper ex = new ExcelHelper();
+            ex.ConfigureHeaders(new Dictionary<string, int>
+            {
+                { /* Header name */ nameof(EmployeeModel.Id),      /* Index */ 1 },
+                { /* Header name */ nameof(EmployeeModel.Name),    /* Index */ 2 },
+                { /* Header name */ nameof(EmployeeModel.Age),     /* Index */ 3 },
+                { /* Header name */ nameof(EmployeeModel.Addess),  /* Index */ 4 },
+                { /* Header name */ nameof(EmployeeModel.Bio),     /* Index */ 5 },
+                
+            });
+
+            ex.ExportToDefaltGeneric<EmployeeModel>("Employee With Generic", users.ToList(), (model, worksheet, rowId) =>
+            {
+                ex.AddCellValue(worksheet, rowId, /* Header name */ nameof(EmployeeModel.Id),         /* Header value */ model.Id);
+                ex.AddCellValue(worksheet, rowId, /* Header name */ nameof(EmployeeModel.Name),       /* Header value */ model.Name);
+                ex.AddCellValue(worksheet, rowId, /* Header name */ nameof(EmployeeModel.Age),        /* Header value */ model.Age);
+                ex.AddCellValue(worksheet, rowId, /* Header name */ nameof(EmployeeModel.Addess),     /* Header value */ model.Addess);
+                ex.AddCellValue(worksheet, rowId, /* Header name */ nameof(EmployeeModel.Bio),        /* Header value */ model.Bio);
+            });
+        }
+
+        /* Generic funcs */
+
+        public void ExportToDefaltGeneric<T>(string v, List<T> employeeModels, Action<T, IXLWorksheet, int> value)
+        {
+            if (_headerMapping == null)
+            {
+                throw new NullReferenceException(nameof(ConfigureHeaders) + " For " + nameof(_headerMapping));
+            }
+
+            using (var workbook = new XLWorkbook())
+            {
+                var worksheet = workbook.Worksheets.Add(v);
+
+                // Add headers
+                foreach (var header in _headerMapping!)
+                {
+                    worksheet.Cell(1, header.Value).Value = header.Key;
+                }
+
+                int currentRow = 2;
+                foreach (var item in employeeModels)
+                {
+                    value.Invoke(item, worksheet, currentRow);
+                    currentRow++;
+                }
+
+                worksheet.Columns().AdjustToContents().AdjustToContents();
+
+                // Save the file
+                using (var stream = new MemoryStream())
+                {
+                    workbook.SaveAs(stream);
+                    var content = stream.ToArray();
+
+                    var fileName = string.Concat("Default-Generic", "_", Guid.NewGuid().ToString("N"), ".xlsx");
+                    var appPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, fileName);
+                    File.WriteAllBytes(appPath, content);
+
+                    Console.WriteLine("Saved the default theme (generic) excel file.");
+                }
+            }
+        }
+
+
+        //Only for generic funcs
+        public void AddCellValue(IXLWorksheet worksheet, int rowId, string headerName, XLCellValue value)
+        {
+            var index = _headerMapping
+                .Where(p => p.Key.Equals(headerName, StringComparison.OrdinalIgnoreCase))
+                .Select(s => s.Value)
+                .FirstOrDefault();
+
+            worksheet.Cell(rowId, index).Value = value;
+        }
+
+
 
     }
 }
